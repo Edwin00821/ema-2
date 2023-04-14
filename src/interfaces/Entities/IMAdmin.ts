e7cba2993f86db7a4c71567d25ae007881a59b5e
Edwin00821 <eastudillop2000@alumno.ipn.mx>
2023-04-14 02:10:14 -0600

    feat: fiz imports of interfaces

--- src/interfaces/Entities/IMAdmin.ts
+++ src/interfaces/Entities/IMAdmin.ts
@@ -1,25 +1,25 @@
-import type { IDUser } from 'interfaces/Entities';
+import type { IDUser } from './'
 
 interface IMAdmin {
-  isLogued?: boolean;
-  num_empleado: string;
-  user?: IDUser;
-  valida_adm: number | string;
-  correo_user: string;
+  isLogued?: boolean
+  num_empleado: string
+  user?: IDUser
+  valida_adm: number | string
+  correo_user: string
 }
 
 interface IMAdminReq {
-  num_empleado: string | null;
-  correo_user: string ;
-  user: IDUser;
-  valida_adm: number | string;
+  num_empleado: string | null
+  correo_user: string
+  user: IDUser
+  valida_adm: number | string
 }
 
 interface IMAdminRes {
-  isLogued?: boolean;
-  num_empleado: string | null;
-  user: IDUser;
-  valida_adm: number | string;
+  isLogued?: boolean
+  num_empleado: string | null
+  user: IDUser
+  valida_adm: number | string
 }
 
-export type { IMAdmin, IMAdminReq, IMAdminRes};
+export type { IMAdmin, IMAdminReq, IMAdminRes }
